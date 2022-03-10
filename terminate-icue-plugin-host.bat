acb6c15ef63d355a6b2cedf5b5e908215630a3cb
Leo <leo.steiner@mesheven.com>
2022-03-11 09:12:28 +1100

    Fixed powershell not exiting when running bat files

--- terminate-icue-plugin-host.bat
+++ terminate-icue-plugin-host.bat
@@ -1,2 +1,2 @@
 @echo off
-Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-executionpolicy bypass', '-NoExit', '-NoLogo', '-File \"%~dp0terminate-icue.ps1\" -pluginhost'}"
+Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-executionpolicy bypass', '-NoLogo', '-File \"%~dp0terminate-icue.ps1\" -pluginhost'}"
